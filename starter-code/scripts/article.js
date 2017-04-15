29694254b088ffeae9cb4a7032af9b6ca251d97a
Redhatfour1 <yaloumessenger@yahoo.com>
2017-04-15 11:52:17 -0700

    called Article.fetchAll method

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -50,7 +50,8 @@ Article.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(); //TODO: What do we pass in to loadAll()?
+    Article.loadAll(JSON.parse(localStorage.rawData)); //TODO: What do we pass in to loadAll()?
+    articleView.initIndexPage();
     //TODO: What method do we call to render the index page?
   } else {
     // TODO: When we don't already have the rawData,
@@ -58,5 +59,13 @@ Article.fetchAll = function() {
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
+    $(()=> {
+      $.ajax({url: '/starter-code/data/hackerIpsum.json'})
+      .done(function(data) {
+        localStorage.setItem('rawData', JSON.stringify(data));
+        Article.loadAll(JSON.parse(localStorage.rawData));
+        articleView.initIndexPage();
+      });
+    });
   }
 }
